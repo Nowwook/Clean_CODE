9dbe9bc5ad5969a858130daedb7b56044241dc7b
Nowwook <110366684+Nowwook@users.noreply.github.com>
2024-07-31 15:49:03 +0900

    Update Singleton.cs

--- GoF 디자인 패턴/Singleton.cs
+++ GoF 디자인 패턴/Singleton.cs
@@ -4,6 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+클래스1, 클래스2 가 있을 때 
+클래스1 생성자에서 클래스2의 인스턴스를 생성
+클래스2 생성자에서 클래스1의 인스턴스를 생성
+클래스1 인스턴스 생성 → 클래스2 인스턴스 생성 → 클래스1 인스턴스 생성 → 계속 반복
+> 스택이 초과되고 StackOverflowException이 발생
+= 싱글톤 사용
+*/
 namespace Singleton_1
 {
     internal class Program
@@ -29,13 +37,12 @@ namespace Singleton_1
     {
         // 객체 선언 못 하게
         private Singleton() { }
-        private static Singleton _instance;
+        private static Singleton _instance;    // 싱글톤 인스턴스를 저장하는 정적 필드
 
-        // 
         private static readonly object _lock = new object();
 
         // _instance 를 사용할 수 있는 method 를 만든다
-        public static Singleton GetInstance()
+        public static Singleton GetInstance()    // 인스턴스에 접근하기 위한 정적 프로퍼티
         {
             // Thread in safe
             if(_instance == null)
